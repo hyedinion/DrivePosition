6df19cf3e834c31e8784768f7c5280993e712fb6
YeoDongHun <64246124+YeoDongHun@users.noreply.github.com>
2021-07-21 08:20:27 +0900

    Update and rename v2_transform_model to v2_transform_model.c

--- transform_model/v2_transform_model.c
+++ transform_model/v2_transform_model.c
@@ -32,10 +32,10 @@ Drivepos transformModel(Drivepos setting, CarModel target, float hip_to_eye)
     
     /* 좌석시트 */
     float A = setting.a_u + setting.model.a_d; // 사용자가 편안하다고 느끼는 공간
-    float delta_a = A - target.a_d; // default에서 사용자가 움직여야하는 x값
+    float delta_a = A - target.a_d; // 바뀐차량에서 사용자가 움직여야하는 x값
     
     float B = - setting.b_u - setting.model.b_d + setting.model.e; // 조정된 시트에서 대쉬보드까지의 높이
-    float delta_b = - B - target.b_d + target.e; // default에서 사용자가 움직여야하는 y값
+    float delta_b = - B - target.b_d + target.e; // 바뀐차량에서 사용자가 움직여야하는 y값
     
     /* 사이드미러 */
     float setting_C = setting.a_u + setting.model.c; // 사이드미러 중앙 ~ 시트설정후 사용자의 눈위치 (차량 옆면과 수평이 되는 거리)
@@ -49,17 +49,16 @@ Drivepos transformModel(Drivepos setting, CarModel target, float hip_to_eye)
              
     float p = 2*setting.lr_angle - atan2(setting_C, setting_D); // 좌우 시야각의 각도 (사이드미러에서 바라봤을때의 좌우시야 각) // 사용자 취향반영 (바깥쪽(>90), 중간(90), 안쪽(<90)..)
     float q = 2*setting.ud_angle + atan2(setting_C, setting_E); // 상하 시야각의 각도 (사이드미러에서 바라봤을때의 상하시야 각) // 사용자 취향반영 (위(>90), 중간(90), 아래(<90)..)
-    // 여기서 설명추가!@!@
     
-    float target_lr_angle
-    float target_ud_angle
+    float delta_lr_angle = (p + atan2(target_C, target_D))/2; // 바뀐차량에서 사용자가 움직여야하는 사이드미러 좌우각도
+    float delta_ud_angle = (q - atan2(target_C, target_E))/2; // 바뀐차량에서 사용자가 움직여야하는 사이드미러 상하각도
     
-    return (Drivepos){delta_a, delta_b, target_lr_angle, target};
+    return (Drivepos){delta_a, delta_b, delta_lr_angle, delta_ud_angle, target};
 }
 
 Drivepos getToMove(Drivepos setting, Drivepos current)
 {
-    return (Drivepos){setting.x - current.x, setting.y - current.y, setting.model};
+    return (Drivepos){setting.a_u - current.a_u, setting.b_u - current.b_u, setting.lr_angle - current.lr_angle, setting.ud_angle - current.ud_angle, setting.model};
 }
 
 /*
@@ -88,38 +87,51 @@ CarModel Grandeur = {3, 3, "Grandeur"}, Morning = {1, 1, "Morning"};
 int main()
 {   
     float hip_to_eye; // 엉덩이 ~ 시야까지의 거리.(키로 부터 일정한 비율로 받아옴)
-    Drivepos setting_Moring= {15, 15, Morning}, current_Grandeur = {8, 20, Grandeur};
+    Drivepos setting_Morning= {15, 15, Morning}, current_Grandeur = {8, 20, Grandeur};
+    
+    // 엉덩이에서 눈높이 까지의 길이
+    printf("Enter your height : "); // 키입력
+    scanf("%f",&hip_to_eye);
+    hip_to_eye *= 0.44 // 키와 엉덩이에서 눈높이까지의 길이에 대한 연관관계
     
     //모닝의 세팅 출력
     printf("setting Morning\n");
-    printf("x : %f\n", setting_Moring.x);
-    printf("y : %f\n", setting_Moring.y);
-    printf("model name : %s\n", setting_Moring.model.name);
+    printf("x : %f\n", setting_Morning.a_u);
+    printf("y : %f\n", setting_Morning.b_u);
+    printf("lr_angle : %f\n", setting_Morning.lr_angle);
+    printf("ud_angle : %f\n", setting_Morning.ud_angle);
+    printf("model name : %s\n", setting_Morning.model.name);
     /*
     setting Morning
     x : 15
     y : 15
+    lr_angle :
+    ud_angle :
     model name : Morning
     */
     printf("\n\n\n");
 
     //모닝의 세팅을 그랜저 세팅으로 변환 후 출력
-    Drivepos setting_Grandeur = transformModel(setting_Moring, Grandeur);
+    Drivepos setting_Grandeur = transformModel(setting_Morning, Grandeur, hip_to_eye);
     printf("setting Grandeur(transformed)\n");
-    printf("x : %f\n", setting_Grandeur.x);
-    printf("y : %f\n", setting_Grandeur.y);
+    printf("x : %f\n", setting_Grandeur.a_u);
+    printf("y : %f\n", setting_Grandeur.b_u);
+    printf("lr_angle : %f\n", setting_Grandeur.lr_angle);
+    printf("ud_angle : %f\n", setting_Grandeur.ud_angle);
     printf("model name : %s\n", setting_Grandeur.model.name);
     /*
     setting Grandeur(transformed)
     x : 13
     y : 13
+    lr_angle :
+    ud_angle :
     model name : Grandeur
     */
     printf("\n\n\n");
 
     //얼마나 움직여야 하는지 출력
     Drivepos toMove = getToMove(setting_Grandeur, current_Grandeur);
-    printf("to move\nx %f\ny %f\n", toMove.x, toMove.y);
+    printf("to move\nx %f\ny %f\nlr_angle %f\nud_angle %f\n", toMove.a_u, toMove.b_u, toMove.lr_angle, toMove.ud_angle);
     /*
     to move
     x 5
